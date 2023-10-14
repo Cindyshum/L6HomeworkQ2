178b1c97dee5f6f8fb3607b46c892c36f74692a4
Cindyshum <146828834+Cindyshum@users.noreply.github.com>
2023-10-15 02:36:43 +0800

    Update app.py

--- app/app.py
+++ app/app.py
@@ -10,19 +10,21 @@ app = Flask(__name__)
 def hello_world():
     request_type_str = request.method
     if request_type_str == 'GET':
-        return render_template('index.html', href2='')
+        return render_template('index.html', href2='', href3='')
     else:
         myage = request.form['age']
         mygender = request.form['gender']
         myacademic = request.form['academic']
-        model = load('app/music-recommender.joblib')
+        model = load('music-recommender.joblib')
         np_arr = np.array([myage, mygender, myacademic])
         predictions = model.predict([np_arr])  
         predictions_to_str = str(predictions)
         #return predictions_to_str
-        return render_template('index.html', href2=
+        return render_template('index.html', 
+                               href2=
                                'The suitable music for you (age:'+str(myage)+
                                ' ,gender:'+str(mygender)+
                                ' ,academic-qualification:' +str(myacademic)+
-                               ' )is: '+predictions_to_str)
-
+                               ' )is: ',
+                               href3=predictions_to_str
+                            )
