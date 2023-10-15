49fd4d44504a9cee319df7629d389d0cfdece3a9
Cindyshum <146828834+Cindyshum@users.noreply.github.com>
2023-10-15 15:39:43 +0800

    Update app.py for debug

--- app/app.py
+++ app/app.py
@@ -16,16 +16,17 @@ def hello_world():
         mygender = request.form['gender']
         myacademic = request.form['academic']
         model = load('app/music-recommender.joblib')
-        np_arr = np.array([myage, mygender, myacademic])
-        # np_arr = [myage, mygender, myacademic]
-        predictions = model.predict([np_arr])  
-        predictions_to_str = str(predictions)
-        #return predictions_to_str
-        return render_template('index.html', 
-                               href2=
-                               'The suitable music for you (age:'+str(myage)+
-                               ' ,gender:'+str(mygender)+
-                               ' ,academic-qualification:' +str(myacademic)+
-                               ' )is: ',
-                               href3=predictions_to_str
-                            )
+        # np_arr = np.array([myage, mygender, myacademic])
+        # # np_arr = [myage, mygender, myacademic]
+        # predictions = model.predict([np_arr])  
+        # predictions_to_str = str(predictions)
+        # #return predictions_to_str
+        # return render_template('index.html', 
+        #                        href2=
+        #                        'The suitable music for you (age:'+str(myage)+
+        #                        ' ,gender:'+str(mygender)+
+        #                        ' ,academic-qualification:' +str(myacademic)+
+        #                        ' )is: ',
+        #                        href3=predictions_to_str
+        #                     )
+        return render_template('index.html', href2='test href2', href2='test href3')
